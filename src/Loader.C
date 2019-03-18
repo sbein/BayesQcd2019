e74752740b1c2bc299c02cb4905580513aa35fa0
Sam Bein <samuel.bein@gmail.com>
2019-03-18 09:43:42 +0100

    deleting unnecessary scripts

--- src/Loader.C
+++
@@ -1,8 +0,0 @@
-// Loader.C
-#include "TLorentzVector.h"
-#include "src/UsefulJet.h"
-#include <vector>
-#ifdef __MAKECINT__
-#pragma link C++ class vector<TLorentzVector>+;
-#pragma link C++ class vector<UsefulJet>+;
-#endif
